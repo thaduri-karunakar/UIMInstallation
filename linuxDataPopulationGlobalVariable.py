d560768843087381d6348d1b8aa7cd50c6dd9a6e
thaduri-karunakar <reddy.karnakar4@gmail.com>
2020-06-05 21:15:31 +0800

    updated linuxDataPopulationGlobalVariable.py with jenkis variables

--- linuxDataPopulationGlobalVariable.py
+++ linuxDataPopulationGlobalVariable.py
@@ -1,12 +1,13 @@
-uim_username = "administrator"
-uim_password = "interOP@123"
-uimserver = '10.17.188.162'
-vm_username = 'root'
-vm_password = 'interOP@123'
-domain = r'/C8SERVER1_domain/C8SERVER1_hub/C8SERVER1'
-uimpath = r'/opt/nimsoft'
+import os
+uim_username = os.getenv("uim_username")
+uim_password = os.getenv("uim_password")
+uimserver = os.getenv("uimserver")
+vm_username = os.getenv("vm_username")
+vm_password = os.getenv("vm_password")
+domain = os.getenv("domain")
+uimpath = os.getenv("uimpath")
 probe_deactivate = r"""{}/bin/pu -u {} -p {} {}/controller probe_deactivate automated_deployment_engine""".format(uimpath, uim_username, uim_password, domain)
 probe_activate = r"""{}/bin/pu -u {} -p {} {}/controller probe_activate automated_deployment_engine""".format(uimpath, uim_username, uim_password, domain)
 probe_deploy = r"""{}/bin/pu -u {} -p {} {}/automated_deployment_engine deploy_probe probename""".format(uimpath, uim_username, uim_password, domain)
-uimversion = 201
+uimversion = os.getenv("uimversion")
 port = 22
