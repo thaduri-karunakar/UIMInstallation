fbb40795b86cad7944257959731e16fed376614d
thaduri-karunakar <reddy.karnakar4@gmail.com>
2020-07-09 19:14:23 +0800

    update windowsDataPopulation.py

--- windowsDatapPopulation.py
+++ windowsDatapPopulation.py
@@ -1,143 +1,158 @@
 import sys
 import time
+import traceback
 import windowsDataPopulationGlobalVariable as gfile
 from pypsexec.client import Client
 
 start = time.time()
-netusecmd = r"net use \\{}\QA\NimBUS-install\Probes\SystemTestingProbes {} /user:{}".format(
-    gfile.fileSharePassword, gfile.fileShareUserName, gfile.fileShareIP)
+netusecmd = r"net use \\{}\QA\NimBUS-install\Probes\SystemTestingProbes {} /user:{}\{}".format(gfile.fileShareIP,
+    gfile.fileSharePassword, gfile.hostName, gfile.fileShareUserName )
 
 
 def remote_connection():
     try:
+        global c
         c = Client(gfile.uimServer, gfile.uimServerLoginName, gfile.uimServerLoginPassword, encrypt='False')
         c.connect()
-        return c
+        c.create_service()
+        # return c
     except Exception as e:
         print('Below exception occured .....\n')
-        type, value, traceback = sys.exc_info()
-        print('Error opening %s: %s' % (value.filename, value.strerror))
-        print()
+        traceback.print_exc()
 
 
 def archive_pkg_copying():
     """ copying packages from LVFILESHARE.dhcp.broadcom.net to UIM server machine"""
     try:
-        remote_connection().create_service()
+        # remote_connection().create_service()
         print('service created for following "{}".......\n\n'.format(gfile.uimServer))
         print("copying packages from LVFILESHARE.dhcp.broadcom.net to UIM server machine")
-        stdout, stderr, rc = remote_connection().run_executable("cmd.exe", arguments='''/c  {}'''.format(netusecmd))
+        stdout, stderr, rc = c.run_executable("cmd.exe", arguments='''/c  {}'''.format(netusecmd))
         stdout = str(stdout, 'utf-8')
         stderr = str(stderr, 'utf-8')
+        print(netusecmd)
         if rc == 0:
             print('net use command executed successfully : ', stdout)
             archive_pkg_copy_cmd = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\*.zip {}\archive".format(gfile.fileShareIP, gfile.uimVersion, gfile.uimPath)
-            stdout1, stderr1, rc1 = remote_connection().run_executable("cmd.exe", arguments='''/c  {} '''.format(archive_pkg_copy_cmd))
+            stdout1, stderr1, rc1 = c.run_executable("cmd.exe", arguments='''/c  {} '''.format(archive_pkg_copy_cmd))
             stdout1 = str(stdout1, 'utf-8')
             stderr1 = str(stderr1, 'utf-8')
             if rc1 == 0:
                 print('archive_pkg_copy_cmd command executed successfully...')
-                stdout2, stderr2, rc2 = remote_connection().run_executable('cmd.exe',
-                                                                           arguments=''' /c {} '''.format(
-                                                                               gfile.probe_deactivate))
+                stdout2, stderr2, rc2 = c.run_executable('cmd.exe', arguments=''' /c {} '''.format(gfile.probe_deactivate))
                 stdout2 = str(stdout2, 'utf-8')
                 stderr2 = str(stderr2, 'utf-8')
+                time.sleep(2)
                 if rc2 == 0:
-                    print('ade_deactivate command executed successfully...')
-                    time.sleep(5)
-                    stdout3, stderr3, rc3 = remote_connection().run_executable('cmd.exe',
-                                                                               arguments=''' /c {} '''.format(
-                                                                                   gfile.probe_activate))
-                    stdout3 = str(stdout3, 'utf-8')
-                    stderr3 = str(stderr3, 'utf-8')
-                    if rc3 == 0:
-                        print('ade_activate command executed successfully...')
+                    if ("_command failed: communication error" not in stdout2) or ("command not found" not in stdout2):
+                        print('ade_deactivate command executed successfully...')
                         time.sleep(5)
-                    else:
-                        print('Failed to execute ade_activate command : ', stderr3)
+                        stdout3, stderr3, rc3 = c.run_executable('cmd.exe', arguments=''' /c {} '''.format(gfile.probe_activate))
+                        stdout3 = str(stdout3, 'utf-8')
+                        stderr3 = str(stderr3, 'utf-8')
+                        if rc3 == 0:
+                            if ("_command failed: communication error" not in stdout3) or ("command not found" not in stdout3):
+                                print('ade_activate command executed successfully...')
+                                time.sleep(10)
+                            else:
+                                print('Failed to execute ade_activate command : ', stderr3)
+                                remote_connection_close()
+                                print("Exit from the program with above issue...")
+                                sys.exit()
 
+                    else:
+                        print('Failed to execute ade_deactivate command : ', stderr2)
+                        remote_connection_close()
+                        print("Exit from the program with above issue...")
+                        sys.exit()
                 else:
                     print('Failed to execute ade_deactivate command : ', stderr2)
+                    remote_connection_close()
+                    print("Exit from the program with above issue...")
+                    sys.exit()
 
             else:
-                print(
-                    'Failed to execute archive_pkg_copy_cmd command  :  {}\n{} '.format(stderr1, archive_pkg_copy_cmd))
+                print('Failed to execute archive_pkg_copy_cmd command  :  {}\n{} '.format(stderr1, archive_pkg_copy_cmd))
+                remote_connection_close()
+                print("Exit from the program with above issue...")
+                sys.exit()
 
         else:
             print('Failed to execute net use command :  {}.......\n {} '.format(stderr, stdout))
+            remote_connection_close()
+            print("Exit from the program with above issue...")
             sys.exit()
 
     except Exception as e:
         print('Below exception occured')
-        print(e)
-        print()
+        traceback.print_exc()
 
 
 def probe_deplyment():
     """ Deploying probes on primary robot of uim server """
     try:
-        remote_connection().create_service()
+        # c.create_service()
         print("Deploying probes on primary robot of uim server")
         for probe in ('cdm', 'dirscan', 'logmon', 'processes', 'net_connect'):
             probe_deploy = gfile.probe_deploy
             probe_deployment = probe_deploy.replace("probename", probe)
-            stdout, stderr, rc = remote_connection().run_executable("cmd.exe",
-                                                                    arguments='''/c  {}'''.format(probe_deployment))
+            print(probe_deployment)
+            stdout, stderr, rc = c.run_executable("cmd.exe", arguments='''/c  {}'''.format(probe_deployment))
             stdout = str(stdout, 'utf-8')
             stderr = str(stderr, 'utf-8')
+            print(rc)
             if rc == 0:
-                print('{} probe deployed successfully...'.format(probe))
-                time.sleep(2)
+                if ("_command failed: communication error" not in stdout) or ("command not found" not in stdout):
+                    print('{} probe deployed successfully:  \n {} \n'.format(probe, stdout), '*' * 43, sep='')
+                    time.sleep(2)
             else:
-                print('Failed to deploy {} probe :   {} '.format(probe, stderr))
-
+                print('Failed to deploy {} probe :   {}\n {} '.format(probe, stdout, rc))
+                remote_connection_close()
+                print("Exit from the program with above issue...")
+                sys.exit()
     except Exception as e:
         print('Below exception occured .....\n')
-        type, value, traceback = sys.exc_info()
-        print('Error opening %s: %s' % (value.filename, value.strerror))
-        # print(e)
-        print()
-
+        traceback.print_exc()
 
 def cfg_replacing():
     """ copying/replacing  cfg files from LVFILESHARE.dhcp.broadcom.net to UIM server machine"""
 
     try:
-        remote_connection().create_service()
-        print("waiting for 30 sec to finish probe deployment....")
+        # c.create_service()
+        print("waiting for 30 sec to finish probe deactivation\n", '*' * 43, sep='')
         time.sleep(30)
         print(r"copying/replacing  cfg files from LVFILESHARE.dhcp.broadcom.net to UIM server machin")
-        cdmcfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\cdm.cfg {}\probes\system\cdm".format(gfile.fileShareIP, gfile.uimversion, gfile.uimPath)
-        stdout, stderr, rc = remote_connection().run_executable("cmd.exe", arguments='''/c  {}'''.format(netusecmd))
-        stdout, stderr, rc = remote_connection().run_executable("cmd.exe", arguments='''/c  {}'''.format(cdmcfg))
+        cdmcfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\cdm.cfg {}\probes\system\cdm".format(gfile.fileShareIP, gfile.uimVersion, gfile.uimPath)
+        stdout, stderr, rc = c.run_executable("cmd.exe", arguments='''/c  {}'''.format(netusecmd))
+        stdout, stderr, rc = c.run_executable("cmd.exe", arguments='''/c  {}'''.format(cdmcfg))
         stdout = str(stdout, 'utf-8')
         stderr = str(stderr, 'utf-8')
         if rc == 0:
             print('CDM cfg file replaced successfully...')
-            dirscancfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\dirscan.cfg {}\probes\system\dirscan".format(gfile.fileShareIP, gfile.uimversion, gfile.uimPath)
-            stdout1, stderr1, rc1 = remote_connection().run_executable("cmd.exe",
+            dirscancfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\dirscan.cfg {}\probes\system\dirscan".format(gfile.fileShareIP, gfile.uimVersion, gfile.uimPath)
+            stdout1, stderr1, rc1 = c.run_executable("cmd.exe",
                                                                        arguments='''/c  {} '''.format(dirscancfg))
             stdout1 = str(stdout1, 'utf-8')
             stderr1 = str(stderr1, 'utf-8')
             if rc1 == 0:
                 print('dirscan cfg file replaced successfully...')
-                logmoncfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\logmon.cfg {}\probes\system\logmon".format(gfile.fileShareIP, gfile.uimversion, gfile.uimPath)
-                stdout2, stderr2, rc2 = remote_connection().run_executable('cmd.exe',
+                logmoncfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\logmon.cfg {}\probes\system\logmon".format(gfile.fileShareIP, gfile.uimVersion, gfile.uimPath)
+                stdout2, stderr2, rc2 = c.run_executable('cmd.exe',
                                                                            arguments=''' /c {} '''.format(logmoncfg))
                 stdout2 = str(stdout2, 'utf-8')
                 stderr2 = str(stderr2, 'utf-8')
                 if rc2 == 0:
                     print('logmon cfg file replaced successfully...')
-                    processescfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\processes.cfg {}\probes\system\processes".format(gfile.fileShareIP, gfile.uimversion, gfile.uimPath)
-                    stdout3, stderr3, rc3 = remote_connection().run_executable('cmd.exe',
+                    processescfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\system\processes.cfg {}\probes\system\processes".format(gfile.fileShareIP, gfile.uimVersion, gfile.uimPath)
+                    stdout3, stderr3, rc3 = c.run_executable('cmd.exe',
                                                                                arguments=''' /c {} '''.format(
                                                                                    processescfg))
                     stdout3 = str(stdout3, 'utf-8')
                     stderr3 = str(stderr3, 'utf-8')
                     if rc3 == 0:
                         print('processes cfg file replaced successfully...')
-                        net_connectcfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\network\net_connect.cfg {}\probes\network\net_connect".format(gfile.fileShareIP, gfile.uimversion, gfile.uimPath)
-                        stdout4, stderr4, rc4 = remote_connection().run_executable('cmd.exe',
+                        net_connectcfg = r"echo All | copy \\{}\qa\NimBUS-install\Probes\SystemTestingProbes\{}\Windows_CFG\network\net_connect.cfg {}\probes\network\net_connect".format(gfile.fileShareIP, gfile.uimVersion, gfile.uimPath)
+                        stdout4, stderr4, rc4 = c.run_executable('cmd.exe',
                                                                                    arguments=''' /c {} '''.format(
                                                                                        net_connectcfg))
                         stdout4 = str(stdout4, 'utf-8')
@@ -146,65 +161,75 @@ def cfg_replacing():
                             print('net_connect cfg file replaced successfully...')
                         else:
                             print("Failed to replace net_connect cfg file :  {}\n{} ".format(stderr4, net_connectcfg))
+                            remote_connection_close()
+                            print("Exit from the program with above issue...")
+                            sys.exit()
 
                     else:
                         print("Failed to replace processes cfg file  :  {}\n{} ".format(stderr3, processescfg))
+                        remote_connection_close()
+                        print("Exit from the program with above issue...")
+                        sys.exit()
 
                 else:
                     print('Failed to replace logmon cfg file  : {}\n{} '.format(stderr2, dirscancfg))
+                    remote_connection_close()
+                    print("Exit from the program with above issue...")
+                    sys.exit()
 
             else:
                 print('Failed to replace dirscan cfg file   :  {}\n{} '.format(stderr1, dirscancfg))
+                remote_connection_close()
+                print("Exit from the program with above issue...")
+                sys.exit()
 
         else:
             print('Failed to replace cdm cfg file :  {}\n {} '.format(stderr, stdout))
+            remote_connection_close()
+            print("Exit from the program with above issue...")
+            sys.exit()
 
     except Exception as e:
         print('Below exception occured .....\n')
-        type, value, traceback = sys.exc_info()
-        print('Error opening %s: %s' % (value.filename, value.strerror))
-        print()
+        traceback.print_exc()
 
 
-def probe_restart():
+def probe_restart(probe_status):
     """ Restarting probes on primary robot of uim server """
     try:
-        remote_connection().create_service()
-
-        print("Restarting probes on primary robot of uim server")
+        print("waiting for 30 sec to finish probe deployment\n", '*' * 43, sep='')
+        time.sleep(30)
+        print("{} on primary robot of uim server\n".format(probe_status), '*' * 43, sep='')
         for probe in ['cdm', 'dirscan', 'logmon', 'processes', 'net_connect']:
-            probe_deactivate = gfile.probe_deactivate
-            probe_deactivate = probe_deactivate.replace("automated_deployment_engine", probe)
-            stdout, stderr, rc = remote_connection().run_executable("cmd.exe",
-                                                                    arguments='''/c  {}'''.format(probe_deactivate))
+            probe_status_change = gfile.probe_status
+            probe_status_change = probe_status_change.replace("automated_deployment_engine", probe)
+            probe_status_change = probe_status_change.replace("probe_status", probe_status)
+            stdout, stderr, rc = c.run_executable("cmd.exe", arguments='''/c  {}'''.format(probe_status_change))
             stdout = str(stdout, 'utf-8')
             stderr = str(stderr, 'utf-8')
             if rc == 0:
-                print('{} probe deactivated successfully...'.format(probe))
-                time.sleep(5)
-                probe_activate = gfile.probe_activate
-                probe_activate = probe_activate.replace("automated_deployment_engine", probe)
-                stdout, stderr, rc = remote_connection().run_executable("cmd.exe",
-                                                                        arguments='''/c  {}'''.format(probe_activate))
-                stdout = str(stdout, 'utf-8')
-                stderr = str(stderr, 'utf-8')
-                if rc == 0:
-                    print('{} probe activated successfully...'.format(probe))
-                    time.sleep(2)
-                else:
-                    print('Failed to activate probe :  {}\n {} '.format(probe, stderr))
-            else:
-                print('Failed to deactivate probe :  {}\n {} '.format(probe, stderr))
+                if ("_command failed: communication error" in stdout) or ("command not found" in stdout):
+                    print('{} failed for probe :  {}\n{}\n'.format(probe_status, probe, stdout), '*' * 43, sep='')
+                    remote_connection_close()
+                    print("Exit from the program with above issue...")
+                    sys.exit()
 
+                else:
+                    print('{}d successfully: {} \n'.format(probe_status, probe), '*' * 43, sep='')
+                    print(stdout, '\n')
+                    time.sleep(5)
+            else:
+                print('{} failed for probe :  {}\n {}\n'.format(probe_status, probe, stderr), '*' * 43, sep='')
+                remote_connection_close()
+                print("Exit from the program with above issue...")
+                sys.exit()
     except Exception as e:
         print('Below exception occured')
-        print(e)
-        print()
-
+        traceback.print_exc()
 
 def remote_connection_close():
-    remote_connection().remove_service()
-    remote_connection().disconnect()
+    c.remove_service()
+    c.disconnect()
     print('service removed for following "{}"'.format(gfile.uimServer))
     print('Script has taken', (time.time() - start) / 60, 'Minuets..')
 
@@ -212,6 +237,7 @@ def remote_connection_close():
 remote_connection()
 archive_pkg_copying()
 probe_deplyment()
+probe_restart('probe_deactivate')
 cfg_replacing()
-probe_restart()
+probe_restart('probe_activate')
 remote_connection_close()
